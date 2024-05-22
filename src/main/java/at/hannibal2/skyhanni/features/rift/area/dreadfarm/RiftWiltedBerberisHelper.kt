8d6d1cc07e950483bbac77009f658dda830742f7
Alexander Wilson <82721356+LynixPlayz@users.noreply.github.com>
2024-05-21 23:17:01 -0500

    huge berberis upgrade

--- src/main/java/at/hannibal2/skyhanni/features/rift/area/dreadfarm/RiftWiltedBerberisHelper.kt
+++ src/main/java/at/hannibal2/skyhanni/features/rift/area/dreadfarm/RiftWiltedBerberisHelper.kt
@@ -1,5 +1,7 @@
 package at.hannibal2.skyhanni.features.rift.area.dreadfarm
 
+import at.hannibal2.skyhanni.data.ClickType
+import at.hannibal2.skyhanni.events.BlockClickEvent
 import at.hannibal2.skyhanni.events.LorenzRenderWorldEvent
 import at.hannibal2.skyhanni.events.LorenzTickEvent
 import at.hannibal2.skyhanni.events.ReceiveParticleEvent
@@ -15,10 +17,12 @@ import at.hannibal2.skyhanni.utils.LorenzVec
 import at.hannibal2.skyhanni.utils.RenderUtils.draw3DLine
 import at.hannibal2.skyhanni.utils.RenderUtils.drawDynamicText
 import at.hannibal2.skyhanni.utils.RenderUtils.drawFilledBoundingBox_nea
+import at.hannibal2.skyhanni.utils.RenderUtils.exactPlayerEyeLocation
 import at.hannibal2.skyhanni.utils.RenderUtils.expandBlock
 import at.hannibal2.skyhanni.utils.toLorenzVec
 import net.minecraft.client.Minecraft
 import net.minecraft.init.Blocks
+import net.minecraft.util.BlockPos
 import net.minecraft.util.EnumParticleTypes
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import java.awt.Color
@@ -57,6 +61,40 @@ class RiftWiltedBerberisHelper {
     private fun nearestBerberis(location: LorenzVec): WiltedBerberis? {
         return list.filter { it.currentParticles.distanceSq(location) < 8 }
             .minByOrNull { it.currentParticles.distanceSq(location) }
+    }
+
+    @SubscribeEvent
+    fun onBlockClick(event: BlockClickEvent) {
+        println("Block Clicked")
+        if (!isEnabled()) return
+        println("1")
+        if (!hasFarmingToolInHand) return
+        println("2")
+
+        val location = event.position
+        println("3")
+        val berberis = nearestBerberis(location)
+        println("4")
+
+        if (event.clickType == ClickType.LEFT_CLICK) {
+            println("5")
+            if (berberis == null) {
+                println("6")
+                list = list.editCopy { add(WiltedBerberis(location)) }
+                println("added wilted berberis")
+                return
+            }
+
+            with(berberis) {
+                println("7")
+                previous = location
+                if (moving) {
+                    println("8")
+                    list = list.editCopy { add(WiltedBerberis(location)) }
+                    println("added wilted berberis")
+                }
+            }
+        }
     }
 
     @SubscribeEvent
@@ -119,24 +157,46 @@ class RiftWiltedBerberisHelper {
                 val location = currentParticles.fixLocation(berberis)
                 if (!moving) {
                     event.drawFilledBoundingBox_nea(axisAlignedBB(location), Color.YELLOW, 0.7f)
+                    event.draw3DLine(event.exactPlayerEyeLocation(), location.add(0.5, 0.0, 0.5), Color.YELLOW, 3, true)
                     event.drawDynamicText(location.add(y = 1), "§eWilted Berberis", 1.5, ignoreBlocks = false)
                 } else {
                     event.drawFilledBoundingBox_nea(axisAlignedBB(location), Color.WHITE, 0.5f)
                     previous?.fixLocation(berberis)?.let {
                         event.drawFilledBoundingBox_nea(axisAlignedBB(it), Color.LIGHT_GRAY, 0.2f)
                         event.draw3DLine(it.add(0.5, 0.0, 0.5), location.add(0.5, 0.0, 0.5), Color.WHITE, 3, false)
-                        event.draw3DLine(it.add(0.5, 0.0, 0.5), location.add(0.5, 0.0, 0.5), Color.MAGENTA, 3, false)
-                        println((location.add(0.5, 0.0, 0.5) - it.add(0.5, 0.0, 0.5)).normalize())
                         for(pos in BlockUtils.traceRay(it.add(0.5, 0.5, 0.5), (location.add(0.5, 0.0, 0.5) - it.add(0.5, 0.0, 0.5)).normalize(), 25.0)) {
-                            println(Minecraft.getMinecraft().theWorld.getBlockState(pos).block)
                             if(Minecraft.getMinecraft().theWorld.getBlockState(pos).block == Blocks.deadbush) {
                                 event.drawFilledBoundingBox_nea(axisAlignedBB(pos.toLorenzVec()), Color.RED, 0.5f)
+                                pos.toLorenzVec().let { it1 ->
+                                    event.draw3DLine(event.exactPlayerEyeLocation(), it1
+                                        .add(0.5, 0.0, 0.5), Color.RED, 3, true)
+                                }
                             }
                         }
                     }
                 }
             }
         }
+    }
+
+    fun getAveragePosition(blockPosList: List<BlockPos>): BlockPos? {
+        if (blockPosList.isEmpty()) return null
+
+        var sumX = 0
+        var sumY = 0
+        var sumZ = 0
+
+        for (blockPos in blockPosList) {
+            sumX += blockPos.x
+            sumY += blockPos.y
+            sumZ += blockPos.z
+        }
+
+        val averageX = sumX / blockPosList.size
+        val averageY = sumY / blockPosList.size
+        val averageZ = sumZ / blockPosList.size
+
+        return BlockPos(averageX, averageY, averageZ)
     }
 
     private fun axisAlignedBB(loc: LorenzVec) = loc.add(0.1, -0.1, 0.1).boundingToOffset(0.8, 1.0, 0.8).expandBlock()
